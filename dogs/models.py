9a07a5bc15be8787ddbce0ec6b3bfe06883e6d95
gaurdianaq <gaurdianaq@protonmail.com>
2025-07-29 22:18:54 -0400

    Added dogs app, created first migrations

---
+++ dogs/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Breed(models.Model):
+    SIZES = {
+        "XS": "Extra Small",
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+        "XL": "Extra Large" 
+    }
+    name = models.CharField(max_length=50)
+    size = models.CharField(max_length=2, choices=SIZES, default="M")
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
